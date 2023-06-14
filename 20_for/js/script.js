90324ecbee30607fccc465dc2d90370c4d6dc37d
unknown <vanessaaraujo77077@gmail.com>
2023-06-14 10:29:09 -0300

    adicionado pasta 19 e 20

---
+++ 20_for/js/script.js
@@ -0,0 +1,20 @@
+for (var i = 0;i < 10; i++) {
+    console.log("repetindo for: " + i)
+}
+
+
+
+//mostrar os elementos que contem no meu array
+var arr = [1,2,3,4,5,6,7,8,9]
+
+for(var j = 0; j < arr.length; j++) {
+    console.log(arr[j])
+}
+
+console.log(arr.length)
+
+
+for(var x = 2;x < 100; x *= 3){
+    console.log(x)
+
+}
